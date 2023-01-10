8607ec10b443cf3d50b22ef6a7055cdd8b940e52
6buck <6buck@informatik.uni-hamburg.de>
2023-01-10 15:18:34 +0100

    Implemented first version of sensor community background service

--- Connectors/Sensor.Community/SensorCommunityService.cs
+++ Connectors/Sensor.Community/SensorCommunityService.cs
@@ -1,34 +1,197 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Web;
 using SensorData.Api.Collector.Models;
 using SensorData.Api.Collector.Services;
 
 namespace SensorData.Api.Collector.Connectors.Sensor.Community;
 
-public class SensorCommunityService : ISensorDataCollector
+public class SensorCommunityService : BackgroundService
 {
     private const string BaseUrl = "https://data.sensor.community/static/v2/data.json";
     private readonly HttpClient _httpClient;
+    private readonly JsonSerializerOptions _options;
+    private Timer? _timer = null;
 
     public SensorCommunityService(HttpClient httpClient)
     {
         _httpClient = httpClient;
 
         // Todo: configure user-agent
-        _httpClient.DefaultRequestHeaders.Add("User-Agent", "Heat-Islands Detection Uni Hamburg 6buck@informatik.uni-hamburg.de");
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", HttpUtility.UrlEncode("Heat-Islands Detection Uni Hamburg 6buck@informatik.uni-hamburg.de"));
+
+        _options = new JsonSerializerOptions();
+        _options.PropertyNameCaseInsensitive = true;
+        _options.Converters.Add(new DateTimeConverterUsingDateTimeParse());
+        // _options.Converters.Add(new DoubleConverterUsingDoubleParse());
     }
 
-    public Task<IEnumerable<SensorDataReading>> GetSensorReadings()
+    public async Task GetSensorReadings(object state)
     {
-        
+        if (state is not TimerContext ctx)
+            return;
+
+        // Get result from api.
+        // Includes all sensor readings of all sensors averaged over the last 5 min.
+        var result = await _httpClient.GetStringAsync(BaseUrl, ctx.Token);
+        // var result = await File.ReadAllTextAsync("readings.json");
+
+        var readings = JsonSerializer.Deserialize<List<SCSensorReading>>(result, _options);
+
+        // Filter out everything we don't need
+        var readingsToSave = readings.Where(r => LocationFilter(r) && SensorFilter(r)).ToList();
+        var readingsToReturn = new List<SensorDataReading>(readingsToSave.Count);
+
+        // Todo: Save readings to the DB
+        Console.WriteLine($"Readings from {DateTime.UtcNow.ToString("s")}");
+
+        foreach (var item in readingsToSave)
+        {
+            readingsToReturn.Add(item.ToSensorDataReading());
+            Console.WriteLine(item);
+        }
     }
 
-    private HttpRequestMessage GetRequestMessage()
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var message = new HttpRequestMessage(HttpMethod.Get, BaseUrl);
+        _timer = new Timer(async state => await GetSensorReadings(state), new TimerContext { Token = stoppingToken }, 0, (int)TimeSpan.FromMinutes(5).TotalMilliseconds);
 
-        return message;
+        return Task.CompletedTask;
+    }
+
+    private bool LocationFilter(SCSensorReading r)
+    {
+        // Bad Oldeslohe: 53.79604022151624N, 10.355624923959386E
+        // Lüneburg: 53.24894326638651, 10.369662928285342
+        // Elmshorn: 53.7886691669084, 9.659963814592672
+        // Tostedt: 53.27786506750316, 9.669322484225408
+
+        return r.Location != null && r.Location.Country == "DE" && r.Location.Indoor == 0
+             && decimal.Parse(r.Location.Latitude) <= 53.8m && decimal.Parse(r.Location.Latitude) >= 53.2m
+             && decimal.Parse(r.Location.Longitude) <= 10.4m && decimal.Parse(r.Location.Longitude) >= 9.6m;
+    }
+
+    private bool SensorFilter(SCSensorReading r)
+    {
+        return true;
+        // return r.Sensor != null && r.Sensor.SensorType != null && r.Sensor.SensorType.Name == "SDS011";
     }
 }
+
+#region Models
+
+public record SCSensorReading
+{
+    public long Id { get; set; }
+    // public object? SamplingRate { get; set; }
+    public DateTime Timestamp { get; set; }
+    public SCLocation Location { get; set; }
+    public SCSensor Sensor { get; set; }
+    public List<SCSensorDataReading> SensorDataValues { get; set; }
+
+    public SensorDataReading ToSensorDataReading()
+    {
+        var reading = new SensorDataReading()
+        {
+            Location = new Location()
+            {
+                Latitude = decimal.Parse(Location.Latitude),
+                Longitude = decimal.Parse(Location.Longitude),
+                Altitude = decimal.Parse(Location.Altitude)
+            }
+        };
+
+        // Map temperature readings.
+        if (Sensor.SensorType.Name == "BME280")
+        {
+            var temperatureDataReading = SensorDataValues.FirstOrDefault(s => s.ValueType == "temperature");
+
+            if (temperatureDataReading != null && double.TryParse(temperatureDataReading.Value.ToString(), out var parsedTempReading))
+            {
+                reading.Temperature = new TemperatureReading()
+                {
+                    Value = parsedTempReading,
+                    Measurement = "Celsius",
+                    SensorType = "BME280"
+                };
+            }
+        }
+
+        return reading;
+    }
+}
+
+public record SCLocation
+{
+    public long Id { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+    public string Altitude { get; set; }
+    public string Country { get; set; }
+
+    [JsonPropertyName("exact_location")]
+    public int ExactLocation { get; set; }
+    public int Indoor { get; set; }
+}
+
+public record SCSensor
+{
+    public long Id { get; set; }
+    public string Pin { get; set; }
+
+    [JsonPropertyName("sensor_type")]
+    public SCSensorType SensorType { get; set; }
+}
+
+public record SCSensorType
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+}
+
+public record SCSensorDataReading
+{
+    public long Id { get; set; }
+    public object Value { get; set; }
+
+    [JsonPropertyName("value_type")]
+    public string ValueType { get; set; }
+}
+
+internal record TimerContext
+{
+    public CancellationToken Token { get; set; }
+}
+
+public class DateTimeConverterUsingDateTimeParse : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.Parse(reader.GetString() ?? string.Empty);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
+
+public class DoubleConverterUsingDoubleParse : JsonConverter<double>
+{
+    public override double Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (double.TryParse(reader.GetString() ?? string.Empty, out var result))
+            return result;
+
+        return default;
+    }
+
+    public override void Write(Utf8JsonWriter writer, double value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
+
+
+#endregion
--- Models/SensorDataReading.cs
+++ Models/SensorDataReading.cs
@@ -5,7 +5,22 @@ using System.Threading.Tasks;
 
 namespace SensorData.Api.Collector.Models;
 
-public class SensorDataReading
+public record SensorDataReading
 {
-    public double Temperature { get; set; }
+    public Location Location { get; set; }
+    public TemperatureReading Temperature { get; set; }
+}
+
+public record TemperatureReading
+{
+    public double Value { get; set; }
+    public string Measurement { get; set; } = "Celsius";
+    public string SensorType { get; set; }
+}
+
+public record Location
+{
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public decimal Altitude { get; set; }
 }
